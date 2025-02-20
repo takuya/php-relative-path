17454db8f7ca0db2a2299aade97ab6b3f3875eb0
takuya <55338+takuya@users.noreply.github.com>
2025-02-20 19:49:36 +0900

    save

--- misc/Helpers/helpers.php
+++ misc/Helpers/helpers.php
@@ -1,24 +1,23 @@
 <?php
-
-
-function dd ( ...$args ) {
-  //var_dump(debug_backtrace());
-  dump( ...$args );
-  exit;
-}
-
-;
-
-function dump ( ...$args ) {
-  echo "\n";
-  foreach ( func_get_args() as $e ) {
-    var_dump( $e );
+if( ! function_exists('dd') ) {
+  function dd( ...$args ) {
+    //var_dump(debug_backtrace());
+    dump(...$args);
+    exit;
   }
 }
-
-;
-
-function ascii_letters(){
-  $ascii_letters = array_map(function($e){return chr($e);},array_merge(range(65,90),range(97,122)));
-  return $ascii_letters;
+if( ! function_exists('dump') ) {
+  function dump( ...$args ) {
+    echo "\n";
+    foreach (func_get_args() as $e) {
+      var_dump($e);
+    }
+  }
+};;
+if( ! function_exists('ascii_letters') ) {
+  function ascii_letters() {
+    $ascii_letters = array_map(function ( $e ) { return chr($e); }, array_merge(range(65, 90), range(97, 122)));
+    
+    return $ascii_letters;
+  }
 }
